f45cb72248015bcea11181092e69c08ad1d670f4
Bruno Vitor Corrêa <brunovitorc@gmail.com>
2023-05-10 11:40:13 -0300

    Update 1.3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import "./styles.css";
 import Header from "./components/Header";
 import Section from "./components/Section";
+import ListItem from "./components/ListItem";
+
+const gamesListData = [
+  {
+    url: "https://www.twitch.tv/directory/game/League%20of%20Legends",
+    imageUrl: "https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg",
+    alt: "Imagem do jogo League of Legends"
+  },
+  {
+    url: "https://www.twitch.tv/directory/game/VALORANT",
+    imageUrl: "https://static-cdn.jtvnw.net/ttv-boxart/516575-188x250.jpg",
+    alt: "Imagem do jogo Valorant"
+  },
+  {
+    url: "https://www.twitch.tv/directory/game/Minecraft",
+    imageUrl: "https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-188x250.jpg",
+    alt: "Imagem do jogo Minecraft"
+  },
+  {
+    url: "https://www.twitch.tv/directory/game/Teamfight%20Tactics",
+    imageUrl: "https://static-cdn.jtvnw.net/ttv-boxart/513143-188x250.jpg",
+    alt: "Imagem do jogo TFT"
+  }
+];
 
 export default function App() {
   return (
@@ -11,7 +35,25 @@ export default function App() {
         <Section
           title="Meus Jogos"
           subtitle="Os games que eu mais curto jogar!"
-        />
+        >
+          {gamesListData.map(function (item) {
+            return (
+              <ListItem
+                url={item.url}
+                imageUrl={item.imageUrl}
+                alt={item.alt}
+              />
+            );
+          })}
+
+          {/* items da lista */}
+          <ListItem
+            url="https://www.twitch.tv/directory/game/League%20of%20Legends"
+            imageUrl="https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg"
+            alt="Imagem do jogo League of Legends"
+          />
+        </Section>
+
         <Section
           title="Canais e Streamers"
           subtitle="Lista de canais e transmissões que não perco!"
--- src/components/Section.js
+++ src/components/Section.js
@@ -4,56 +4,7 @@ export default function Section(props) {
       <div>
         <h2>{props.title}</h2>
         <p>{props.subtitle}</p>
-        <ul class="games-list">
-          <li>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.twitch.tv/directory/game/League%20of%20Legends"
-            >
-              <img
-                src="https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg"
-                alt="Imagem do jogo League of Legends"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.twitch.tv/directory/game/VALORANT"
-            >
-              <img
-                src="https://static-cdn.jtvnw.net/ttv-boxart/516575-188x250.jpg"
-                alt="Imagem do jogo Valorant"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.twitch.tv/directory/game/Minecraft"
-            >
-              <img
-                src="https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-188x250.jpg"
-                alt="Imagem do jogo Minecraft"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.twitch.tv/directory/game/Teamfight%20Tactics"
-            >
-              <img
-                src="https://static-cdn.jtvnw.net/ttv-boxart/513143-188x250.jpg"
-                alt="Imagem do jogo TFT"
-              />
-            </a>
-          </li>
-        </ul>
+        <ul class="games-list">{props.children}</ul>
       </div>
     </section>
   );
